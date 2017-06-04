70059099e81f69411246be94a3b1f9f19be4d0f0
tewksbg <GregATewksbury@gmail.com>
2017-06-04 18:44:16 -0400

    Update and rename config.example.php to config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$GLOBALS['config'] = [
+    // DATABASE
+    'DBHOST' => 'db684894332.db.1and1.com',
+    'DBNAME' => 'db684894332',
+    'DBUSER' => 'dbo684894332',
+    'DBPASS' => '1Rootbeer8'
+];
+
+
+?>
